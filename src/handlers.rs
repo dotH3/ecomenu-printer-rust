6e1fc1ae44fe22a9bbda17ecfa28f39c51f692a2
dotH3 <joaquinbastias321@gmail.com>
2025-04-04 23:17:31 -0300

    Remove build script and clean up Cargo.toml; enhance print_request function for better logging

--- src/handlers.rs
+++ src/handlers.rs
@@ -8,5 +8,18 @@ pub fn hello(param: String, agent: String) -> String {
 }
 
 pub fn print_request(body: serde_json::Value) -> String {
+    println!("Received POST request with body: {:?}", body);
+    
+    let printer = printers::get_default_printer();
+
+    if printer.is_some() {
+        let printer = printer.unwrap();
+        println!("Printer: {:?}", printer);
+        printer.print("Hola Martina".as_bytes(), None).unwrap();
+    } else {
+        println!("No default printer found");
+    }
     format!("Received POST request with body: {}", body)
+
+
 }
